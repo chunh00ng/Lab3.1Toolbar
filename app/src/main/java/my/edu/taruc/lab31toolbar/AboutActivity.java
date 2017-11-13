0fef3e0eb7219eb7a67b712360f978a19d4e74cb
chunh00ng <leech-wa15@student.tarc.edu.my>
2017-11-13 13:29:03 +0800

    adasad

---
+++ app/src/main/java/my/edu/taruc/lab31toolbar/AboutActivity.java
@@ -0,0 +1,19 @@
+package my.edu.taruc.lab31toolbar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        //WebView webViewAbout = (WebView)findViewById(R.id.webViewAbout);
+        WebView webView = new WebView(this);
+        setContentView(webView);
+        webView.loadUrl("https://www.google.com/");
+    }
+}
